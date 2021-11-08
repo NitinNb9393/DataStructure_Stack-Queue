5c6c2c9245fe3fcf0430cda9f1855119d4e3599a
NitinNb9393 <nitinbabar9393@gmail.com>
2021-11-08 15:10:38 +0530

    Program for the Create the Stack

---
+++ src/com/birdgelabz/datastructure_stack_queue/Node.java
@@ -0,0 +1,12 @@
+package com.birdgelabz.datastructure_stack_queue;
+
+public class Node {
+	public int data;
+	public Node next;
+
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+
+	}
+}
